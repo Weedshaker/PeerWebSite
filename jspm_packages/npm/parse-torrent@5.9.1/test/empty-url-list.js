e8399123e3475b94d7c90b863fbc9e7217fb0161
Silvan Strübi <silvan.struebi@mgb.ch>
2020-04-29 00:53:24 +0200

    update
    Signed-off-by: Silvan Strübi <silvan.struebi@mgb.ch>

---
+++ jspm_packages/npm/parse-torrent@5.9.1/test/empty-url-list.js
@@ -0,0 +1,11 @@
+/* */ 
+var fs = require('fs');
+var parseTorrent = require('../index');
+var path = require('path');
+var test = require('tape');
+var leavesUrlList = fs.readFileSync(path.join(__dirname, 'torrents/leaves-empty-url-list.torrent'));
+test('parse empty url-list', function(t) {
+  var torrent = parseTorrent(leavesUrlList);
+  t.deepEqual(torrent.urlList, []);
+  t.end();
+});
