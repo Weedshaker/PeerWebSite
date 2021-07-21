b250700b63578f9abf1d9c18809e246fb268c9eb
Silvan Strübi <silvan.struebi@mgb.ch>
2021-07-21 11:39:28 +0200

    removed minify, which destoryed wworker

--- bash/Live.sh
+++ bash/Live.sh
@@ -3,6 +3,6 @@ source PATH.sh
 cd ../
 #jspm bundle-sfx ./config.js + ./JavaScript/js/init.js ./JavaScript/webRTC.js --minify
 jspm bundle-sfx ./config.js + ./JavaScript/js/init.js ./JavaScript/webRTC.js
-minify ./JavaScript/webRTC.js --out-file ./JavaScript/webRTC.js --mangle false
+#minify ./JavaScript/webRTC.js --out-file ./JavaScript/webRTC.js --mangle false NOTE: Minify destroys global vars inside the WebWorker EncryptDecrypt.js
 # http://jspm.io/0.17-beta-guide/static-builds-with-rollup-optimization.html
 #jspm build ./JavaScript/js/init.js ./JavaScript/webRTC.js
