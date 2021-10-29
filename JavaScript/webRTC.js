5d298f8692a46d776678fb8da4d7bcd8fc0d82d7
Silvan Strübi <silvan.struebi@mgb.ch>
2021-10-29 19:45:59 +0200

    firefox bug fix

--- JavaScript/webRTC.js
+++ JavaScript/webRTC.js
@@ -15217,7 +15217,7 @@ $__System.register('2b', ['5', '6', '7', '27', '29', 'a'], function (_export) {
 						switch (name) {
 							case 'open-or-join-room':
 								this.idNames = ['txt-roomid', 'open-or-join-room', 'sender', 'receiver'];
-								var header = $('<header class="down isTop">\n\t\t\t\t\t<div id="info" class="flex">\n\t\t\t\t\t\t<div class="offline">YOU ARE OFFLINE!!!</div>\n\t\t\t\t\t\t<iframe class="gh-button" src="https://ghbtns.com/github-btn.html?user=Weedshaker&amp;repo=PeerWebSite&amp;type=star&amp;count=true&amp;size=large" scrolling="0" width="160px" height="30px" frameborder="0"></iframe>\n\t\t\t\t\t\t<a href="https://github.com/Weedshaker/PeerWebSite" class="tiny" style="color:white">v. beta 0.8.7<span id="sw-version"></span>; Visit Github for more Infos!</a>\n\t\t\t\t\t\t<a href="' + location.href.replace(location.hash, '') + '" class="recycle">&#9851;&nbsp;<span class="tiny">New Site</span></a>\n\t\t\t\t\t</div>\n\t\t\t\t</header>');
+								var header = $('<header class="down isTop">\n\t\t\t\t\t<div id="info" class="flex">\n\t\t\t\t\t\t<div class="offline">YOU ARE OFFLINE!!!</div>\n\t\t\t\t\t\t<iframe class="gh-button" src="https://ghbtns.com/github-btn.html?user=Weedshaker&amp;repo=PeerWebSite&amp;type=star&amp;count=true&amp;size=large" scrolling="0" width="160px" height="30px" frameborder="0"></iframe>\n\t\t\t\t\t\t<a href="https://github.com/Weedshaker/PeerWebSite" class="tiny" style="color:white">v. beta 0.8.8<span id="sw-version"></span>; Visit Github for more Infos!</a>\n\t\t\t\t\t\t<a href="' + location.href.replace(location.hash, '') + '" class="recycle">&#9851;&nbsp;<span class="tiny">New Site</span></a>\n\t\t\t\t\t</div>\n\t\t\t\t</header>');
 								// add edit
 								header.find('#info').append('<a href="#" class="edit">&#9997;&nbsp;<span class="tiny">' + (!isSender ? 'Edit!' : 'Abort Editing!') + '</span></a>');
 								header.find('.edit').click(function (event) {
@@ -15287,9 +15287,11 @@ $__System.register('2b', ['5', '6', '7', '27', '29', 'a'], function (_export) {
 								this.containers.push(controls);
 								// main containers
 								this.loadingAnimation = '<span class="blobLoading ' + (this.parent.checkHashType(location.hash) === 'magnet' ? 'torrentLoading' : this.parent.checkHashType(location.hash) === 'ipfs' ? 'ipfsLoading' : '') + '"></span><span class="blobLoadingText">Please, be patient. Decentralized content can take a while to load...</span>';
-								var sender = $('<div id="' + this.idNames[2] + '">' + (window.sst && window.sst.karma ? '' : isSender && (this.parent.checkHashType(location.hash) !== 'ipfs' || !/="http.*?gateway\.ipfs\.io.*?#js.*?"/g.test(localStorage.getItem(location.hash))) /* ipfs javascript does not get triggered else */ ? localStorage.getItem(location.hash) || '' : '') + '</div>');
+								var ipfsRegex = /="http.*?gateway\.ipfs\.io.*?#js.*?"/g;
+								var notIpfs = this.parent.checkHashType(location.hash) !== 'ipfs' || !ipfsRegex.test(localStorage.getItem(location.hash)); /* ipfs javascript does not get triggered else */
+								var sender = $('<div id="' + this.idNames[2] + '">' + (isSender && notIpfs ? localStorage.getItem(location.hash) || '' : '') + '</div>');
 								this.containers.push(sender);
-								var receiver = $('<div id="' + this.idNames[3] + '">' + (window.sst && window.sst.karma ? '' : isSender ? 'WEBRTC response...' : this.parent.checkHashType(location.hash) !== 'ipfs' || !/="http.*?gateway\.ipfs\.io.*?#js.*?"/g.test(localStorage.getItem(location.hash)) /* ipfs javascript does not get triggered else */ ? localStorage.getItem(location.hash) || this.loadingAnimation : this.loadingAnimation) + '</div>');
+								var receiver = $('<div id="' + this.idNames[3] + '">' + (isSender ? 'WEBRTC response...' : notIpfs ? localStorage.getItem(location.hash) || this.loadingAnimation : this.loadingAnimation) + '</div>');
 								this.containers.push(receiver);
 								// hot-reloader
 								/*
